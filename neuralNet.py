8f30631244be08c9a6fa2d5e74d2686485faa25f
OverRatedTech <rchavezj@ucsc.edu>
2017-11-02 17:54:15 -0700

    Import libraries

--- neuralNet.py
+++ neuralNet.py
@@ -16,7 +16,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import math
+import math  
+
 import tensorflow as tf
 
 NUM_CLASSES = 20
